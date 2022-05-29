a3af1255c63e6aa5a8d8d52ddbc65a54c916f7e5
Lomikukus <104350475+Lomikukus@users.noreply.github.com>
2022-05-29 12:32:25 +0200

    Ist der server aus teil 2 -K

---
+++ Server.py
@@ -0,0 +1,45 @@
+import socket
+from _thread import *
+import sys
+
+
+server = "192.168.2.108"  # WICHTIG: hier müsst ihr eure locale ip adresse einfügen
+port = 5555  # port mit welchen hier verbunden wird (ruter abhängig kann sein das es ein anderer ist)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)  # die zahl bestimmt wie viele leute Connecten können, hier 2
+print("Warte auf verbindung, Server wurde gestartet")
+
+def threaded_client(conn):
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply= data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+
+
+while True:
+    conn, addr =s.accept()
+    print("Verbunden mit: ", addr)
+
+    start_new_thread(threaded_client,  (conn,))
+
