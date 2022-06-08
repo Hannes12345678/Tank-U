ef1febe3207ef4cd0484cf817b75aef94e79a38b
Hannes <hannes.sattler@student.reutlingen-university.de>
2022-06-08 16:53:03 +0200

    Willi push dich

--- prototyp python test2 von Hannes/tank_Network.py
+++ prototyp python test2 von Hannes/tank_Network.py
@@ -1,6 +1,8 @@
 import socket
 import pickle
 
+import pygame
+
 
 class Network:
     def __init__(self):
@@ -16,13 +18,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(2048))
+            return pickle.loads(self.client.recv(8192))
         except:
             pass
 
     def send(self, data):
         try:
             self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(2048))
+            return pickle.loads(self.client.recv(8192))
         except socket.error as e:
             print(e)
