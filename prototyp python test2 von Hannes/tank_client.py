6f78d3edfd31cafc05b171678b93f135960a346e
Hannes <hannes.sattler@student.reutlingen-university.de>
2022-06-08 19:32:31 +0200

    Willi push dich endlich

--- prototyp python test2 von Hannes/tank_client.py
+++ prototyp python test2 von Hannes/tank_client.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Client")
 blauer_panzer = pygame.image.load("Blue-Tank-V1.png")
 roter_panzer =pygame.image.load("red-tank-v1-benutzen.png")
 blaues_hartes_ding = pygame.image.load("Blue-tank-Kanone-v1-nutzen.png")
+blaues_hartes_ding.set_colorkey((255,255,255))
 rotes_hartes_ding = pygame.image.load("red-tank-kanone-v1-nutzen.png")
 blaues_hartes_ding_turned = pygame.image.load("Blue-Kanone-turned.png")
 rotes_hartes_ding_turned = pygame.image.load("red-Kanone-turned.png")
@@ -31,6 +32,7 @@ def main():
     t = n.getP()
     clock = pygame.time.Clock()
 
+    barrelrotation = 0
     while run:
         clock.tick(30)  #30 fps kamen von Konrad und Hannes deshalb ok wenn die Idee von Willi käme dann hätten wäre es fatal und falsch
         p2 = n.send(p)
@@ -50,9 +52,23 @@ def main():
         print(p.y)
         print(p2.x)
         print(p2.y)
-        print(t)
         redrawWindow(win, p, p2)
-        win.blit(blaues_hartes_ding, (p.x + 15 , p.y ))
+        blaueshartesding = pygame.transform.rotate(blaues_hartes_ding, barrelrotation)
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            barrelrotation = barrelrotation -5
+
+        if keys[pygame.K_DOWN]:
+            barrelrotation = barrelrotation +5
+
+
+
+
+        win.blit(blaueshartesding, ( p.x + 45 - int(blaueshartesding.get_width() / 2), p.y + 80 - int(blaueshartesding.get_height() / 2)))
+
+
         win.blit(rotes_hartes_ding_turned, (p2.x + 15, p2.y +75))
         win.blit(roter_panzer, (p2.x, p2.y))
         win.blit(blauer_panzer, (p.x, p.y))
