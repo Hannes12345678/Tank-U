520e2e9f5d7168f40b2dad06b81ee5afae42d83b
Lomikukus <104350475+Lomikukus@users.noreply.github.com>
2022-06-28 17:20:48 +0200

    barrel rotation mit leichter winkel einstellung der flugbahn, wir heut abend noch feinjustiert. -K

--- prototyp python test2 von Hannes/tank_client.py
+++ prototyp python test2 von Hannes/tank_client.py
@@ -17,6 +17,11 @@ t = n.getP()
 bullety = p.y
 bulletx = p.x
 bullet_staerke = 15 # hier immer ungerade
+bullet_winkel = 0 # davor 10
+
+faky = 5  # faktoren der fariabelnen für x und
+fakx = 5
+
 
 bullet_state = False
 bullet_shoot = False
@@ -106,7 +111,7 @@ def fire_bullet(x,y):
 def stop_bullet():
     global bullet_state
     bullet_state = False
-    print("aufhören")
+    #print("aufhören")
 
 def bullet_weite():
     global bullet_staerke
@@ -121,6 +126,47 @@ def bullet_kurz():
 
     if bullet_staerke < 5:
         bullet_staerke = bullet_staerke + 2
+
+def bullet_grad_plus():
+    global bullet_winkel
+    global fakx
+    global faky
+    bullet_winkel = bullet_winkel + 3
+
+    if bullet_winkel < 18 and faky <= 18:
+        faky = faky + 3
+        if fakx > 2:
+            fakx = fakx - 3
+        elif fakx > 18:
+            fakx = 5
+            faky = 5
+        else:
+            fakx = fakx + 3
+
+def bullet_grad_minus():
+    global bullet_winkel
+    global fakx
+    global faky
+    bullet_winkel = bullet_winkel - 3
+    if bullet_winkel > 0 and fakx <= 18:
+        fakx = fakx + 3
+        if faky > -1:
+            faky = faky - 3
+        elif fakx >18:
+            fakx = 5
+            faky = 5
+        else:
+            faky = faky + 3
+
+
+
+def reset_bullet_trajectory():
+    global fakx
+    global faky
+    global bullet_winkel
+    fakx = 5
+    faky = 5
+    bullet_winkel =0
 
 def redrawWindow(win, player, player2):
     win.fill((255, 255, 255))
@@ -144,7 +190,7 @@ def main():
     t = n.getP()"""
     clock = pygame.time.Clock()
 
-    barrelrotation = 180
+    barrelrotation = 230
     while run:
         clock.tick(20)  #30 fps kamen von Konrad und Hannes deshalb ok wenn die Idee von Willi käme dann hätten wäre es fatal und falsch
         p2 = n.send(p)
@@ -192,24 +238,45 @@ def main():
             blaueshartesding = pygame.transform.rotate(blaues_hartes_ding_turned, barrelrotation)
             keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_UP] and barrelrotation <= 230:
+
+            variX = 0 #wie stark das zeug ist bzw winkel
+            variY = 0
+            #faky = 0  # faktoren der fariabelnen für x und y
+            #fakx = 0
+
+
+            if keys[pygame.K_UP] and barrelrotation <= 240:
                 barrelrotation = barrelrotation + 5
+                bullet_grad_plus()
 
             if keys[pygame.K_DOWN] and barrelrotation >= 180:
                 barrelrotation = barrelrotation - 5
+                bullet_grad_minus()
 
             win.blit(blaueshartesding, (p.x + 85- int(blaueshartesding.get_width() / 2), p.y + 75  - int(blaueshartesding.get_height() / 2)))
 
             win.blit(rotes_hartes_ding_turned, (p2.x + 15, p2.y + 75 ))
             #print(barrelrotation)
 #hier shooting mech p1
-            if keys[pygame.K_w]:
+            if keys[pygame.K_w]:  #flugdauer + bzw vershiebt den hochpunkt
                 bullet_weite()
-                print(bullet_staerke)
+                #print('Staerke: '+ bullet_staerke)
 
-            if keys[pygame.K_s]:
+            if keys[pygame.K_s]: #flugdauer -
                 bullet_kurz()
-                print(bullet_staerke)
+                #print('Staerke: '+ bullet_staerke)
+
+            if keys[pygame.K_q]:
+                bullet_grad_plus()
+                #print('Winkel: '+ bullet_winkel)
+
+            if keys[pygame.K_a]:
+                bullet_grad_minus()
+                #print('Winkel: '+bullet_winkel)
+            if keys[pygame.K_r]:
+                reset_bullet_trajectory()
+                barrelrotation = 230
+
 
 
             if keys[pygame.K_SPACE]:
@@ -221,8 +288,7 @@ def main():
 
 
             if bullet_state is True:
-                 variX = 0
-                 variY = 0
+
                  bulletx = p.x
                  bullety = p.y
                  bullet_shoot = True
@@ -233,16 +299,16 @@ def main():
 
                      for x in range(bullet_staerke * 2) :
 
-                        variX = variX + 5
+                        variX = variX + (fakx)
 
                         distanz = distanz + 2
 
                      # if  else für ab hälfte das andere
                         if distanz < (bullet_staerke  ):
-                            variY = variY - 5
+                            variY = variY - (faky)
                             fire_bullet(bulletx + variX, bullety + variY)
                         elif distanz > (bullet_staerke  ):# and distanz != (bullet_staerke*4)
-                            variY = variY + 10
+                            variY = variY + (faky) #war mal 5
                             fire_bullet(bulletx + variX, bullety + variY)
                         elif distanz == bullet_staerke:
 
@@ -255,7 +321,7 @@ def main():
 
 
                      fire_bullet(bulletx + variX, bullety + variY)
-                     if variX > 200:
+                     if variX > 400:
                          bullet_shoot = False
                          stop_bullet()
 
