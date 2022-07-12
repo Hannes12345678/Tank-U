2faae8175687b2b2ffe9d4c6512ecc279d27f025
willi <willi.siedlaczek@t-online.de>
2022-07-12 12:44:18 +0200

    Hitbox fixes

--- prototyp python test2 von Hannes/tank_Network.py
+++ prototyp python test2 von Hannes/tank_Network.py
@@ -5,25 +5,25 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "" # hier ip adresse einfügen
+        self.server = "192.168.178.96" #192.168.2.108
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
 
 
-    def getP(self): #damit pplayer dinge übertragen werden
+    def getP(self):
         return self.p
 
 
 #connection überprüfung
-    def connect(self): #connction
+    def connect(self):
         try:
             self.client.connect(self.addr)
             return pickle.loads(self.client.recv(8192))
         except:
             pass
 #senden daten
-    def send(self, data): #pickle von daten
+    def send(self, data):
 
         try:
             self.client.send(pickle.dumps(data))
