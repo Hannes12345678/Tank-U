8f9a86535ede5994447d294bdc8fd01f6af08f8d
Lomikukus <104350475+Lomikukus@users.noreply.github.com>
2022-07-15 09:52:39 +0200

    Fertig kommentiert - K

--- prototyp python test2 von Hannes/tank_Network.py
+++ prototyp python test2 von Hannes/tank_Network.py
@@ -5,25 +5,25 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.178.96" #192.168.2.108
+        self.server = "" # hier ip adresse einfügen
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
 
 
-    def getP(self):
+    def getP(self): #damit pplayer dinge übertragen werden
         return self.p
 
 
 #connection überprüfung
-    def connect(self):
+    def connect(self): #connction
         try:
             self.client.connect(self.addr)
             return pickle.loads(self.client.recv(8192))
         except:
             pass
 #senden daten
-    def send(self, data):
+    def send(self, data): #pickle von daten
 
         try:
             self.client.send(pickle.dumps(data))
